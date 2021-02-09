ac6921f1e96205fae786407bd10688dd97481ae2
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-09 11:30:03 -0600

    loaders

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+const loaders = require('./loaders');
+
 const { PORT } = require('./config');
 
 async function startServer() {
+
+    // Init application loaders
+    loaders(app);
+
+    // Start server
     app.listen(PORT, () => {
         console.log(`Server listening on PORT ${PORT}`);
     })
