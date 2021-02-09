7d53e116f89454cfe757f64e578fc64d37e48fe8
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-09 12:32:06 -0600

    passport loader in index

--- loaders/index.js
+++ loaders/index.js
@@ -1,5 +1,5 @@
 const expressLoader = require('./express');
-//const passportLoader = require('./passport');
+const passportLoader = require('./passport');
 //const routeLoader = require('../routes');
 const swaggerLoader = require('./swagger');
 const swagger = require('./swagger');
@@ -10,7 +10,7 @@ module.exports = async (app) => {
     const expressApp = await expressLoader(app);
 
     // Load Passport middleware
-    //const passport = await passportLoader(expressApp);
+    const passport = await passportLoader(expressApp);
 
     // Load API route handlers
     //await routeLoader(app, passport);
