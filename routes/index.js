1ba866e2329255dafe1bc82d66d21397adae5be9
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-09 16:16:17 -0600

    user router

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const authRouter = require('./auth');
 // const cartRouter = require('./cart');
 // const orderRouter = require('./order');
 // const productRouter = require('./product');
-// const userRouter = require('./user');
+const userRouter = require('./user');
 
 module.exports = (app, passport) => {
    authRouter(app, passport);
 //   cartRouter(app);
 //   orderRouter(app);
 //   productRouter(app);
-//   userRouter(app);
+   userRouter(app);
 }
