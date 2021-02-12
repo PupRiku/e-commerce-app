4216e61d940e8acee3ba1de99e82c92ab22e1688
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-11 22:49:47 -0600

    config for deployment

--- db/index.js
+++ db/index.js
@@ -2,13 +2,15 @@
 
 const { Pool } = require('pg');
 const { DB } = require('../config');
+require("dotenv").config();
+
+const devConfig = `postgresql://${DB.PG_USER}:${DB.PG_PASSWORD}@${DB.PG_HOST}:${DB.PG_PORT}/${DB.PG_DATABASE}`;
+
+const proConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Pool({
-  user: DB.PGUSER,
-  host: DB.PGHOST,
-  database: DB.PGDATABASE,
-  password: DB.PGPASSWORD,
-  port: DB.PGPORT
+    connectionString:
+      process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 
 module.exports = {
